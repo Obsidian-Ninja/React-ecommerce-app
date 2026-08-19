4adf295bf44f8e2510c093437f8855c33102c7e0
Gurvansh Gupta <gurvxnsh.gupta@gmail.com>
2026-08-20 02:57:26 +0530

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
-import NewsletterBox from '../components/NewsLetterBox.jsx'
+import NewsLetterBox from '../components/NewsLetterBox.jsx'
 
 const About = () => {
   return (
